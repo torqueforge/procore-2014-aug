949cdbaa3d4bb11ab1d9fbccaacf70bb917f4ba8
Sandi Metz <skmetz@gmail.com>
2014-08-27 11:59:31 -0700

    DRY bottles

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -1,18 +1,46 @@
 class Bottles
   def verse(number)
-    case number
-    when 0
-      "No more bottles of beer on the wall, no more bottles of beer.\n" +
-      "Go to the store and buy some more, 99 bottles of beer on the wall.\n"
-    when 1
-      "1 bottle of beer on the wall, 1 bottle of beer.\n" +
-      "Take it down and pass it around, no more bottles of beer on the wall.\n"
-    when 2
-      "2 bottles of beer on the wall, 2 bottles of beer.\n" +
-      "Take one down and pass it around, 1 bottle of beer on the wall.\n"
+    "#{quantity(number).capitalize} #{container(number)} of beer on the wall, #{quantity(number)} #{container(number)} of beer.\n" +
+    "#{action(number)}, #{quantity(successor(number))} #{container(successor(number))} of beer on the wall.\n"
+  end
+
+  def action(number)
+    if number == 0
+      "Go to the store and buy some more"
     else
-      "#{number} bottles of beer on the wall, #{number} bottles of beer.\n" +
-      "Take one down and pass it around, #{number - 1} bottles of beer on the wall.\n"
+      "Take #{pronoun(number)} down and pass it around"
+    end
+  end
+
+  def successor(number)
+    if number == 0
+      99
+    else
+      number - 1
+    end
+  end
+
+  def pronoun(number)
+    if number == 1
+      "it"
+    else
+      "one"
+    end
+  end
+
+  def quantity(number)
+    if number == 0
+      "no more"
+    else
+      "#{number}"
+    end
+  end
+
+  def container(number)
+    if number == 1
+      "bottle"
+    else
+      "bottles"
     end
   end
 
